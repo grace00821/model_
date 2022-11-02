d94d68f6b99a86245278e914ea5ea72dfab18ffd
Parachutes <mashichao@Machiavelli.local>
2022-11-02 19:35:32 +0800

    ausculto page

--- src/views/AuscultoView.vue
+++ src/views/AuscultoView.vue
@@ -1,10 +1,6 @@
 <template>
+  <TopTitle title="ausculto" :return-arrow="true" />
   <v-container>
-    <h4 style="font-weight: bolder; text-align: center">
-      <v-icon class="float-left" @click="navigateBack">mdi-arrow-left</v-icon>
-      ausculto
-    </h4>
-    <PlaceHolder />
     <v-btn block @click="startRecord">Record</v-btn>
     <PlaceHolder />
     <v-btn block @click="stopRecord">Stop</v-btn>
@@ -16,6 +12,10 @@
 <script>
 import Recorder from "recorder-js";
 import PlaceHolder from "@/components/PlaceHolder";
+import TopTitle from "@/components/TopTitle";
+import * as tf from "@tensorflow/tfjs";
+const MODEL_R_PATH = "/models/recognition/model.json";
+
 let recorder;
 let gumStream;
 let downloadUrl;
@@ -29,16 +29,22 @@ let audioContext;
 
 export default {
   name: "AuscultoView",
-  components: { PlaceHolder },
+  components: { TopTitle, PlaceHolder },
   data() {
-    return {};
+    return {
+      timer: null,
+      sampleRate: null,
+    };
   },
+  async mounted() {},
   methods: {
     navigateBack() {
       history.back();
     },
     async initMic() {
       audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+      this.sampleRate = audioContext.sampleRate;
 
       audioDom = document.createElement("audio");
 
@@ -62,7 +68,29 @@ export default {
           console.log("Uh oh... unable to get stream...", err);
         });
 
-      await recorder.start();
+      await recorder.start().then(() => {
+        this.timer = setInterval(() => this.pauseAndStartListening(), 1000);
+      });
+    },
+    async pauseAndStartListening() {
+      await recorder
+        .stop()
+        .then(({ blob, buffer }) => {
+          console.log(blob);
+          const downSampledArray = this.downSample(
+            tf
+              .buffer([buffer[0].length], "float32", buffer[0])
+              .toTensor()
+              .arraySync(),
+            1000
+          );
+          const heartDetectArray = this.cropAndPad(downSampledArray);
+          this.doPrediction(heartDetectArray).then((res) => {
+            // The prediction is printed out here.
+            console.log(res.arraySync()[0]);
+          });
+        })
+        .then(await recorder.start());
     },
     startRecord() {
       console.log("Start Recording");
@@ -80,6 +108,61 @@ export default {
         console.log(buffer);
       });
       gumStream.getAudioTracks()[0].stop();
+    },
+    downSample(array, sr) {
+      const sampleRate = this.sampleRate;
+      const mod = Math.round(sampleRate / sr);
+      let result = [];
+      for (let i = 0; i < array.length; i++) {
+        if (i % mod === 0) {
+          result.push(array[i]);
+        }
+      }
+      return result;
+    },
+    cropAndPad(array, sr = 1000) {
+      if (array.length === sr) {
+        return array;
+      } else if (array.length < sr) {
+        while (array.length < sr) array.push(0);
+        return array;
+      } else {
+        return array.slice(0, sr);
+      }
+    },
+    async doPrediction(tensor) {
+      const min = Math.min(...tensor);
+      const max = Math.max(...tensor);
+
+      if (min === 0 && max === 0) {
+        console.log("The mic is not available, no sound recorded");
+        return tf.tensor([0]);
+      } else {
+        let standardTensor = this.minmaxScale(tensor);
+        standardTensor = tf.tensor(standardTensor);
+        standardTensor = standardTensor.reshape([
+          1,
+          standardTensor.shape[0],
+          1,
+        ]);
+        let output = "";
+        await tf.loadLayersModel(MODEL_R_PATH).then((res) => {
+          output = res.predict(standardTensor);
+        });
+        // output.print();
+        return new Promise((resolve) => {
+          resolve(output);
+        });
+      }
+    },
+    minmaxScale(row) {
+      let scaledRow = [];
+      const min = Math.min(...row);
+      const dif = Math.max(...row) - min;
+      row.forEach((element) => {
+        scaledRow.push((element - min) / dif);
+      });
+      return scaledRow;
     },
   },
 };
