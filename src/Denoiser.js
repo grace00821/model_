584babbc4d06d39c5f9f50cee0aa7fd50f22fa36
Grace Yang <grace00821@naver.com>
2022-11-04 06:11:28 +0800

    Denoiser and Despiker

---
+++ src/Denoiser.js
@@ -0,0 +1,22 @@
+import wt from "discrete-wavelets";
+
+import { resample } from "wave-resampler";
+
+class Denoiser {
+  constructor(inputOri) {
+    let coeffs = wt.wavedec([...inputOri], "db10", "symmetric", 7);
+    this.coeffs = coeffs[0];
+  }
+
+  dwtDenoise() {
+    let resampled = resample(this.coeffs.slice(0, 3460), 346, 10000, {
+      method: "sinc",
+    });
+    let resampledLow = resample(this.coeffs.slice(0, 3460), 346, 100, {
+      method: "sinc",
+    });
+    return [resampled, resampledLow];
+  }
+}
+
+export default Denoiser;
--- src/views/AuscultoView.vue
+++ src/views/AuscultoView.vue
@@ -6,6 +6,8 @@
     <v-btn block @click="stopRecord">Stop</v-btn>
     <PlaceHolder />
     <div id="audioPlayer"></div>
+    <v-btn block @click="shareAudio">Share</v-btn>
+    <PlaceHolder />
   </v-container>
 </template>
 
@@ -14,13 +16,21 @@ import Recorder from "recorder-js";
 import PlaceHolder from "@/components/PlaceHolder";
 import TopTitle from "@/components/TopTitle";
 import * as tf from "@tensorflow/tfjs";
+import Denoiser from "@/Denoiser";
+import Despiker from "@/Despiker";
+import { resample } from "wave-resampler";
+import bufferToWav from "audiobuffer-to-wav";
+
 const MODEL_R_PATH = "/models/recognition/model.json";
 
 let recorder;
+let recorder2;
 let gumStream;
+let gumStream2;
 let downloadUrl;
 let audioDom;
 let audioContext;
+let audioBlob;
 
 // recorder: null,
 //     gumStream: null,
@@ -34,6 +44,12 @@ export default {
     return {
       timer: null,
       sampleRate: null,
+      // for heart beat detection
+      heartDetectArray: null,
+      prediction: null,
+      // for length
+      hsRecording: null,
+      hsBlobs: null,
     };
   },
   async mounted() {},
@@ -57,22 +73,28 @@ export default {
       recorder = new Recorder(audioContext, {
         numChannels: 1,
       });
+      recorder2 = new Recorder(audioContext, {
+        numChannels: 1,
+      });
 
       await navigator.mediaDevices
         .getUserMedia({ audio: true })
         .then(function (stream) {
           recorder.init(stream);
+          recorder2.init(stream);
           gumStream = stream;
+          gumStream2 = stream;
         })
         .catch((err) => {
           console.log("Uh oh... unable to get stream...", err);
         });
-
+      await recorder2.start();
       await recorder.start().then(() => {
         this.timer = setInterval(() => this.pauseAndStartListening(), 1000);
       });
     },
     async pauseAndStartListening() {
+      console.log("pause&start");
       await recorder
         .stop()
         .then(({ blob, buffer }) => {
@@ -84,11 +106,13 @@ export default {
               .arraySync(),
             1000
           );
-          const heartDetectArray = this.cropAndPad(downSampledArray);
-          this.doPrediction(heartDetectArray).then((res) => {
+          this.heartDetectArray = this.cropAndPad(downSampledArray);
+          this.doPrediction(this.heartDetectArray).then((res) => {
             // The prediction is printed out here.
             console.log(res.arraySync()[0]);
           });
+          this.hsRecording.push(...this.heartDetectArray);
+          this.hsBlobs.push(blob);
         })
         .then(await recorder.start());
     },
@@ -97,17 +121,61 @@ export default {
       this.initMic();
     },
     async stopRecord() {
-      document.getElementById("audioPlayer").appendChild(audioDom);
+      await clearInterval(this.timer);
       console.log("Stop Recording");
+      // await recorder.stop();
       await recorder.stop().then(({ blob, buffer }) => {
+        document.getElementById("audioPlayer").appendChild(audioDom);
+
+        const originalArray = tf
+          .buffer([buffer[0].length], "float32", buffer[0])
+          .toTensor()
+          .arraySync();
+        const cropAndPadArray = this.cropAndPad(
+          originalArray,
+          audioContext.sampleRate * 10
+        );
+
+        let denoiser = new Denoiser(cropAndPadArray);
+        const denoised = denoiser.dwtDenoise();
+        console.log("denoised");
+        console.log(denoised);
+
+        let despiker = new Despiker(denoised[0]);
+        const despiked = despiker.despike();
+        console.log("despiked");
+        console.log(despiked);
+
+        let upsampled = resample(despiked, 10000, audioContext.sampleRate, {
+          method: "sinc",
+        });
+        console.log("upsampled");
+        console.log(upsampled);
+        console.log("sampleRate");
+        console.log(audioContext.sampleRate);
+
+        let audioBuffer = audioContext.createBuffer(
+          1,
+          upsampled.length,
+          audioContext.sampleRate
+        );
+
+        let wav = bufferToWav(audioBuffer);
+        let newblob = new window.Blob([new DataView(wav)], {
+          type: "audio/wav",
+        });
+
+        blob = newblob;
+
         downloadUrl = URL.createObjectURL(blob);
-        console.log(downloadUrl);
+        audioBlob = blob;
         audioDom.controls = true;
-        audioDom.src = URL.createObjectURL(blob);
-        console.log(blob);
-        console.log(buffer);
+        audioDom.src = downloadUrl;
       });
       gumStream.getAudioTracks()[0].stop();
+      console.log("gumStream stop");
+      gumStream2.getAudioTracks()[0].stop();
+      console.log("gumStream2 stop");
     },
     downSample(array, sr) {
       const sampleRate = this.sampleRate;
@@ -118,9 +186,11 @@ export default {
           result.push(array[i]);
         }
       }
+      console.log("downSample ran");
       return result;
     },
     cropAndPad(array, sr = 1000) {
+      console.log("cropAndPad ran");
       if (array.length === sr) {
         return array;
       } else if (array.length < sr) {
@@ -163,6 +233,25 @@ export default {
         scaledRow.push((element - min) / dif);
       });
       return scaledRow;
+    },
+    shareAudio() {
+      let audioFile = new File([audioBlob], "audio.wav", {
+        type: "audio/wav",
+      });
+      let filesArray = [audioFile];
+      if (
+        navigator.canShare &&
+        this.navigator.canShare({ files: filesArray })
+      ) {
+        navigator
+          .share({
+            files: filesArray,
+          })
+          .then(() => console.log("Share was successful."))
+          .catch((error) => console.log("Sharing failed", error));
+      } else {
+        console.log("Your system doesn't support sharing files.");
+      }
     },
   },
 };
